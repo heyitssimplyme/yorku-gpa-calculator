aa50922a21b92f10871525da061b9bc1995450a6
simplyme <76823487+heyitssimplyme@users.noreply.github.com>
2023-01-05 12:51:09 -0500

    cleanup

--- gpa-calc.js
+++ gpa-calc.js
@@ -1,49 +1,40 @@
 //  by simplyme#9265
 (() => {
-  let points = 0
-  let fourPoint = 0
-  let credits = 0
-
-  const ninePoints = {
-    'A+' : 9.0,
-    'A'  : 8.0,
-    'B+' : 7.0,
-    'B'  : 6.0,
-    'C+' : 5.0,
-    'C'  : 4.0,
-    'D+' : 3.0,
-    'D'  : 2.0,
-    'E'  : 1.0,
-    'F'  : 0.0,
+  const byLetterGrade = {
+    'A+': { nine: 9.0, four: 4.0 },
+    'A' : { nine: 8.0, four: 3.8 },
+    'B+': { nine: 7.0, four: 3.3 },
+    'B' : { nine: 6.0, four: 3.0 },
+    'C+': { nine: 5.0, four: 2.3 },
+    'C' : { nine: 4.0, four: 2.0 },
+    'D+': { nine: 3.0, four: 1.3 },
+    'D' : { nine: 2.0, four: 1.0 },
+    'E' : { nine: 1.0, four: 0.7 },
+    'F' : { nine: 0.0, four: 0.0 },
   }
 
-  const fourPoints = {
-    'A+' : 4.0,
-    'A'  : 3.8,
-    'B+' : 3.3,
-    'B'  : 3.0,
-    'C+' : 2.3,
-    'C'  : 2.0,
-    'D+' : 1.3,
-    'D'  : 1.0,
-    'E'  : 0.7,
-    'F'  : 0.0,
-  }
+  // Skip the first row (header)
+  const rows = Array.from(document.querySelector('table.bodytext').rows).slice(1)
 
-  const rows = document.querySelector('table.bodytext').rows
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i]
+  const { ninePoint, fourPoint, credits } = rows.reduce((acc, row) => {
     const [,courseCell,,gradeCell] = row.cells
-
+  
     const weight = Number(courseCell.innerText.split(' ')[3])
     const grade = gradeCell.innerText.trim()
-
-    if (!!grade && grade !== 'P' && ninePoints.hasOwnProperty(grade)) {
-      points += ninePoints[grade] * weight
-      fourPoint += fourPoints[grade] * weight
-      credits += weight
+  
+    if (Boolean(grade) && grade !== 'P' && byLetterGrade.hasOwnProperty(grade)) {
+      const { nine, four } = byLetterGrade[grade]
+  
+      acc.ninePoint += nine * weight
+      acc.fourPoint += four * weight
+      acc.credits += weight
     }
-  }
+  
+    return acc
+  }, { ninePoint: 0, fourPoint: 0, credits: 0 })
 
-  alert(`GPA: ${(points / credits).toFixed(3)} (9 point)\nGPA: ${(fourPoint / credits).toFixed(3)} (4 point)`)
+  const ninePointGpa = ninePoint / credits
+  const fourPointGpa = fourPoint / credits
+
+  alert(`9 Point GPA: ${ninePointGpa.toFixed(3)}\n4 Point GPA: ${fourPointGpa.toFixed(3)}`)
 })()
